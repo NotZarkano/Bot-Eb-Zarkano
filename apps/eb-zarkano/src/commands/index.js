bb8cf3c9b9e2de355a46b443a02ccf28f83def7f
NotZarkano <kauadisasrzgamer2010@gmail.com>
2026-09-06 12:32:59 -0300

    Update index.js

--- apps/eb-zarkano/src/commands/index.js
+++ apps/eb-zarkano/src/commands/index.js
@@ -1,11 +1,9 @@
-import * as disableModeration from "./disableModeration.js";
 import * as lockChannel from "./lockChannel.js";
 import * as ping from "./ping.js";
 import * as testWelcome from "./testWelcome.js";
 import * as zarkano from "./zarkano.js";
 
 export const commands = new Map([
-  [disableModeration.data.name, disableModeration],
   [lockChannel.data.name, lockChannel],
   [ping.data.name, ping],
   [testWelcome.data.name, testWelcome],
