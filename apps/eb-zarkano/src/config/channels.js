1f9709fcbb2bce6e8d52d5edf6c11035aa375ae0
NotZarkano <kauadisasrzgamer2010@gmail.com>
2026-09-06 18:19:15 +0000

    informação canal

--- apps/eb-zarkano/src/config/channels.js
+++ apps/eb-zarkano/src/config/channels.js
@@ -6,7 +6,7 @@
  */
 export const channelIds = Object.freeze({
   boasVindas: "1543335998509809694",
-  informacoes: "1543337064404361317",
+  informacoes: "1546222671329759403",
   logs: "1546212495537213460",
   recrutamento: "1543620131911893042",
   tickets: "1546216237497843843",
